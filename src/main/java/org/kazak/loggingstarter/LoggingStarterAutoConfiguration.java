4c843a0c92866f0f0a1a28d2aea12c091035a089
KosKazzz <xuzzzz@gmail.com>
2025-02-16 22:11:12 +0300

    initial commit

---
+++ src/main/java/org/kazak/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,7 @@
+package org.kazak.loggingstarter;
+
+public class LoggingStarterAutoConfiguration {
+    public static void println(String str) {
+        System.out.println("Print line from logging starter gradle : " + str);
+    }
+}
